6a7e6033f56576ae68e25490343f86e911df28e5
Lucía Martín <alumno13civica@gmail.com>
2024-12-05 09:43:54 +0000

    User snapshot

--- snapshots/users_snapshot.sql
+++ snapshots/users_snapshot.sql
@@ -5,22 +5,12 @@
       target_schema='snapshots',
       unique_key='user_id',
       strategy='timestamp',
-      updated_at = 'updated_at_UTC',
+      updated_at = 'updated_at',
       invalidate_hard_deletes = true
     )
 }}
 
-select 
-    user_id,
-    first_name,
-    last_name,
-    address_id,
-    phone_number,
-    email,
-    created_at_UTC,
-    updated_at_UTC,
-    is_deleted,
-    date_load_UTC
+select *
 from {{ source('sql_server_dbo', 'users') }}
 
 {% endsnapshot %}
